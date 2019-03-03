301043fc218b9190fb8861d9a692ee1d7e8a9fd4
Pitarn Harnpanichpun <pharn@sertiscorp.com>
2019-03-03 15:13:24 +0700

    learn more

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-
+	// cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
-
 	// hand.print()
 	// remainingCards.print()
 
-	fmt.Println(cards.toString())
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
