a8ef9f4733e4a4476115c1e35df89c4108cf47a1
Pitarn Harnpanichpun <pharn@sertiscorp.com>
2019-06-22 17:00:48 +0700

    learn more

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("https://google.com")
@@ -12,6 +15,17 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
 
-	fmt.Println(resp)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote many lines of bytes", len(bs))
+	return len(bs), nil
 }
