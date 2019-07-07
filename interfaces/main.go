12436c7083406d4ea89cc814e5d96fa135b6fcf4
Pitarn Harnpanichpun <pharn@sertiscorp.com>
2019-07-07 14:56:26 +0700

    finish golang course

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,28 +2,38 @@ package main
 
 import "fmt"
 
-type englishBot struct{}
-type spanishBot struct{}
-type bot interface {
-	getGreeting() string
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	height float64
+	base   float64
+}
+type area interface {
+	getArea() float64
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func printArea(a area) {
+	fmt.Println(a.getArea())
 }
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	sq := square{
+		sideLength: 10,
+	}
+	tri := triangle{
+		base:   10,
+		height: 20,
+	}
+	printArea(sq)
+	printArea(tri)
 
-	printGreeting(eb)
-	printGreeting(sb)
-}
-
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
-
-func (englishBot) getGreeting() string {
-	return "Hi there!"
-}
-
-func (spanishBot) getGreeting() string {
-	return "Hola!"
 }
